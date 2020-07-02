a091c84a2308850f1ad72e406d1c476caf46f0c1
李俊浩 <andi123c16@gmail.com>
2020-07-02 15:43:47 +0800

    add login and admin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
-import { Button , message} from 'antd';
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin';
 
 export default class App extends Component {
 
-  handleClick =()=>{
-    message.success('This is a success message');
-  }
 
   render() {
     return(
-        <Button type="primary" onClick={this.handleClick}>Primary Button</Button>
+        <BrowserRouter>
+            <Switch >
+                <Route path='/login' component={Login}></Route>
+                <Route path={'/' || 'home'} component={Admin}></Route>
+            </Switch>
+        </BrowserRouter>
     )
 
   }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+//manage pages
+export default class Admin extends  Component{
+	render() {
+		return(
+			<div> Admin </div>
+		)
+
+	}
+}
