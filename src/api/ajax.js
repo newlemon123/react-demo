eed2ca2e83081fb6ff41bf2f79ba48f2c57051f1
李俊浩 <andi123c16@gmail.com>
2020-07-03 17:41:24 +0800

    login page

---
+++ src/api/ajax.js
@@ -0,0 +1,10 @@
+/*
+able to send async ajax function
+pack the axios
+function back is promise target
+ */
+import axios from 'axios'
+
+export default function ajax(url, data={},type='GET') {
+
+}
